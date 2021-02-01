b1d4aeff7042f414e688c2d34335ebf3c3f2f4e6
lang-sky <langsky121@gmail.com>
2021-01-31 21:02:21 -0500

    .eslintcache removed

--- react-client/src/components/Site/Messages/index.js
+++ react-client/src/components/Site/Messages/index.js
@@ -1,20 +1,6 @@
-import React, { useState, useEffect, lazy } from 'react';
-import { useQuery, useMutation, useSubscription } from '@apollo/client';
+import React, { lazy } from 'react';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import IconButton from '@material-ui/core/IconButton';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import moment from 'moment';
 import { useSelector } from 'react-redux';
-
-import { MESSAGES_SUBSCRIPTION } from '../../../clients/graphql/message';
 
 const MessageWriter = lazy(() => import('./MessageWriter'));
 const MessagesList = lazy(() => import('./MessagesList'));
